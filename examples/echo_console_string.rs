c8d4f0b848f2598f3f3f87a0d7698d4fef641579
Paul Gilbert <pdgilbert@users.noreply.github.com>
2020-07-09 23:28:19 -0400

    fixed echo_console_string.rs so all bluepill examples build again. echo_console_string uses dma read but not dma write, there is some problem doing that.

--- examples/echo_console_string.rs
+++ examples/echo_console_string.rs
@@ -19,14 +19,15 @@ use cortex_m::singleton;
 use cortex_m_rt::entry;
 use core::fmt::Write;  // for writeln, but not supported by stm32f3xx_hal
 use cortex_m_semihosting::hprintln;
-use nb::block;
+//use nb::block;
 
 use eg_stm_hal::to_str;
 
 #[cfg(feature = "stm32f1xx")]  //  eg blue pill stm32f103
 use stm32f1xx_hal::{prelude::*,   
                     pac::Peripherals, 
-                    serial::{Config, Serial, StopBits, Tx, Rx},  
+                    serial::{Config, Serial, StopBits, Tx, Rx},
+		    dma::{RxDma, dma1::{C5}},     //TxDma,  C4, 
 		    device::USART1 }; 
 
 #[cfg(feature = "stm32f3xx")]  //  eg Discovery-stm32f303
@@ -57,15 +58,17 @@ fn main() -> ! {
     //see serial_char.rs and  echo_console_by_char.rs for additional comments.
     
     #[cfg(feature = "stm32f1xx")]
-    fn serial1_setup() ->  (Tx<USART1>, Rx<USART1>)  {
-
+    fn serial1_setup() ->  (Tx<USART1>, RxDma<Rx<USART1>, C5>)  {
+       
+       // with TxDma return    TxDma<Tx<USART1>, C4>
        let p = Peripherals::take().unwrap();
        let mut rcc = p.RCC.constrain();  
        let clocks = rcc.cfgr.freeze(&mut p.FLASH.constrain().acr); 
        let mut afio = p.AFIO.constrain(&mut rcc.apb2);
        let mut gpioa = p.GPIOA.split(&mut rcc.apb2);
 
-       let (tx1, rx1) = Serial::usart1(
+       //let (tx1, rx1) = Serial::usart1(
+       let txrx1 = Serial::usart1(
 	   p.USART1,
 	   (gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh),     //tx pa9, 
             gpioa.pa10),					    //rx pa10
@@ -73,10 +76,11 @@ fn main() -> ! {
 	   Config::default() .baudrate(9600.bps()) .stopbits(StopBits::STOP1),
 	   clocks,
 	   &mut rcc.apb2,
-	   ).split();
+	   );  //.split();
 
        let channels = p.DMA1.split(&mut rcc.ahb);
-       let tx1 = tx1.with_dma(channels.4);
+       let (tx1, rx1)  = txrx1.split();
+       //let tx1 = tx1.with_dma(channels.4);
        let rx1 = rx1.with_dma(channels.5);
        (tx1, rx1)
        };
@@ -150,45 +154,44 @@ fn main() -> ! {
 
     // End of hal/MCU specific setup. Following should be generic code.
 
-    let (mut tx1, mut rx1) = serial1_setup();
+    let (mut tx1, rx1) = serial1_setup();
+
+    let mut rx1buf = (singleton!(: [u8; 15] = [0; 15]).unwrap(), rx1);
+    //let mut tx1buf = (singleton!(: [u8; 15] = [0; 15]).unwrap(), tx1);
 
     hprintln!("test write to console ...").unwrap();
-    writeln!(tx1, "\r\ncheck console output.\r\n").unwrap();
-    //for byte in b"\r\nconsole connect check.\r\n" { block!(tx1.write(*byte)).ok(); }
-    //let (_, tx1) = tx1.write(b"The quick brown fox").wait();
 
-    //let mut buf = [0u8; 64];
-    let buf = singleton!(: [u8; 15] = [0; 15]).unwrap();
-    let mut bufrx = (buf,  rx1);
+    // writeln! does not work with TxDma
+    writeln!(tx1, "\r\ncheck console output.\r\n").unwrap();
+    // and without dma next expects u8 not &[u8; 25]
+    //let (_, tx1) = tx1.write(b"\r\ncheck console output.\r\n").wait();
+    //let tx1buf = tx1buf.1.write(b"\r\ncheck console output.\r\n").wait();
+
 
     hprintln!("Enter 15 characters in console. Repeat.").unwrap();
     hprintln!("Use ^C in gdb to exit.").unwrap();
 
     writeln!(tx1, "\r\nEnter 15 characters here in the console. Repeat.\r\n").unwrap();
-    //for byte in b"\r\nEnter 15 characters below. Repeat.\r\n" { block!(tx1.write(*byte)).ok(); }
-    //let (_, tx1) = tx1.write(b"\r\nEnter 15 characters below. Repeat.\r\n").wait();
+    //let (_, tx1) =  tx1.write(b"\r\nEnter 15 characters below. Repeat.\r\n").wait();
+    //let tx1buf = tx1buf.1.write(b"\r\nEnter 15 characters below. Repeat.\r\n").wait();
 
-    //#[cfg(feature = "stm32f1xx")]
-    bufrx = bufrx.1.read().wait();  
-    //else
-    //bufrx = rx1.read();
-    //rx1.read(&mut buf)
+    rx1buf = rx1buf.1.read(rx1buf.0).wait();
    
-    hprintln!("received {:?}", to_str(bufrx.0)).unwrap();
-    writeln!(tx1, "{}", to_str(bufrx.0)).unwrap();
-    //bt = tx1.write(bufrx.0).wait(); 
-    //let (_, mut tx1) = tx1.write(bufrx.0).wait();
+    hprintln!("received {:?}", to_str(rx1buf.0)).unwrap();
+    writeln!(tx1, "{}", to_str(rx1buf.0)).unwrap();
+    //let (_, tx1) = tx1.write(rx1buf.0).wait();
+    //let mut x = rx1buf.0.clone();
+    //tx1buf = tx1buf.1.write(& mut x).wait();
+    //tx1buf = tx1buf.1.write(rx1buf.0).wait();
 
-    // cannot get this to work in loop as (buf, rx1), there seem to be circular problems
-    // with move/borrow/mut  but something like this works ...
- 
+    // cannot get loop to work with (buf, rx1), there seem to be circular problems
+    // with move/borrow/mut  but rx1buf structure works ...
+
     //each pass in loop waits for input of 15 chars typed in console
     loop { 
-       //#[cfg(feature = "stm32f1xx")]  //removing ... not supported in this position
-       bufrx = bufrx.1.read(bufrx.0).wait();  
-       //else
-       //bufrx = bufrx.read();
-       hprintln!("received {:?}", to_str(bufrx.0)).unwrap();
-       writeln!(tx1, "{}", to_str(bufrx.0)).unwrap();
+       rx1buf = rx1buf.1.read(rx1buf.0).wait();
+       hprintln!("received {:?}", to_str(rx1buf.0)).unwrap();
+       //tx1buf = tx1buf.1.write(rx1buf.0).wait();
+       writeln!(tx1, "{}", to_str(rx1buf.0)).unwrap();
        }
 }
