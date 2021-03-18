e27afd63b187902e87feb84f041ae56355c2757d
Paul Gilbert <pdgilbert@users.noreply.github.com>
2021-03-18 10:09:07 -0400

    formatted lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! misc small utilities
 //! eg to_str()
-//!   hprintln!("Check 1 string:{}", to_str("my test string".as_bytes())).unwrap(); 
-//!   hprintln!("Check 2 bytes:{:?}",       "myS test string".as_bytes()).unwrap(); 
+//!   hprintln!("Check 1 string:{}", to_str("my test string".as_bytes())).unwrap();
+//!   hprintln!("Check 2 bytes:{:?}",       "myS test string".as_bytes()).unwrap();
 
 #![deny(unsafe_code)]
 #![no_std]
@@ -11,14 +11,13 @@
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-
 //fn from_utf8_lossy...
 
-pub fn to_str( x:&[u8] ) -> &str {
-   match core::str::from_utf8(x) {
-      Ok(str)     => &str,
-      Err(_error) => "problem converting u8 to str ",
-   }
+pub fn to_str(x: &[u8]) -> &str {
+    match core::str::from_utf8(x) {
+        Ok(str) => &str,
+        Err(_error) => "problem converting u8 to str ",
+    }
 }
 
 //      Err(_error) => "problem converting u8 to str ",
@@ -36,14 +35,17 @@ pub fn to_str( x:&[u8] ) -> &str {
 mod tests {
     #[test]
     fn test1() {
-      assert_eq!("my first test string", to_str("my first test string".as_bytes()));
+        assert_eq!(
+            "my first test string",
+            to_str("my first test string".as_bytes())
+        );
     }
 }
 //    #[test]
-//    hprintln!("Check 3 [u8; 8] bytes:{:?}", singleton!(: [u8; 8] = [44, 186, 53, 19, 114, 54, 76, 202]).unwrap()).unwrap(); 
-//    hprintln!("Check 4 [u8; 8] string:{}", to_str(singleton!(: [u8; 8] = [106, 117, 115, 116, 32, 99, 104, 101]).unwrap())).unwrap(); 
-    
-    // these are failing, string that cannot be converted
-    //hprintln!("{}", to_str(singleton!(: [u8; 8] = [44, 186, 53, 19, 114, 54, 76, 202]).unwrap())); 
-    //hprintln!("{}", to_str(singleton!(: [u8; 4] = [44, 186, 53, 19]).unwrap())); 
-    //hprintln!("{}", to_str(singleton!(: [u8; 4] = [114, 54, 76, 202]).unwrap())); 
+//    hprintln!("Check 3 [u8; 8] bytes:{:?}", singleton!(: [u8; 8] = [44, 186, 53, 19, 114, 54, 76, 202]).unwrap()).unwrap();
+//    hprintln!("Check 4 [u8; 8] string:{}", to_str(singleton!(: [u8; 8] = [106, 117, 115, 116, 32, 99, 104, 101]).unwrap())).unwrap();
+
+// these are failing, string that cannot be converted
+//hprintln!("{}", to_str(singleton!(: [u8; 8] = [44, 186, 53, 19, 114, 54, 76, 202]).unwrap()));
+//hprintln!("{}", to_str(singleton!(: [u8; 4] = [44, 186, 53, 19]).unwrap()));
+//hprintln!("{}", to_str(singleton!(: [u8; 4] = [114, 54, 76, 202]).unwrap()));
